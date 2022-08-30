ce1e358ac493ac9732f819ff633bbdd19efa3332
Andres Correa <acorrea@tiresia.co>
2022-08-30 11:48:01 -0500

    Homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,15 +1,23 @@
-'use strict'
-
 function BinarioADecimal(num) {
-  // tu codigo aca
+  let suma = 0;
+  let posicion = 0;
+  let array = num.split('');
 
+  for (let i = array.length -1; i >= 0; i--) {
+    suma += array[i] * Math.pow(2, posicion);
+    posicion++
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let resultado = []
+  while (num > 0){
+    resultado.unshift(num %2);
+    num = Math.floor(num/2);
+  }
+  return resultado.join('')
 }
-
 
 module.exports = {
   BinarioADecimal,
